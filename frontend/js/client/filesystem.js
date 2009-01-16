a86875151913553a55b5b90c1865cb28c884c05f
Ben Galbraith <ben@mozilla.com>
2009-01-16 15:51:06 -0800

    Replaced tabs with four spaces

--- frontend/js/client/filesystem.js
+++ frontend/js/client/filesystem.js
@@ -27,70 +27,70 @@
  * Remote Web Service File System
  */
 var FileSystem = Class.create({
-	
-	newFile: function(project, path, callback) {
-		this.whenFileDoesNotExist(project, path, { 
-			execute: function() {
-				_editSession.startSession(project, path || "new.txt");
-				callback();
-			}, 
-			elseFailed: function() {
-				document.fire("bespin:cmdline:showinfo", { msg: 'The file ' + path + ' already exists my friend.'});
-			}
-		});
-	},
 
-	loadFile: function(project, path, callback) {
-		_editSession.startSession(project, path);
+    newFile: function(project, path, callback) {
+        this.whenFileDoesNotExist(project, path, {
+            execute: function() {
+                _editSession.startSession(project, path || "new.txt");
+                callback();
+            },
+            elseFailed: function() {
+                document.fire("bespin:cmdline:showinfo", { msg: 'The file ' + path + ' already exists my friend.'});
+            }
+        });
+    },
 
-		_server.loadFile(project, path, function(content) {
-			if (content.endsWith("\n")) content = content.substr(0, content.length - 1);
-	
-			callback({
-				name: path,
-				content: content,
-				timestamp: new Date().getTime()
-			});
-		});
-	},
+    loadFile: function(project, path, callback) {
+        _editSession.startSession(project, path);
 
-	projects: function(callback) {
-		_server.projects(callback);
-	},
+        _server.loadFile(project, path, function(content) {
+            if (content.endsWith("\n")) content = content.substr(0, content.length - 1);
 
-	fileNames: function(project, callback) {
-	    _server.list(project, '', callback);
-	},
+            callback({
+                name: path,
+                content: content,
+                timestamp: new Date().getTime()
+            });
+        });
+    },
 
-	saveFile: function(project, file) {
-		// Unix files should always have a trailing new-line; add if not present
-		if (!file.content.endsWith("\n")) file.content += "\n";
+    projects: function(callback) {
+        _server.projects(callback);
+    },
 
-		_server.saveFile(project, file.name, file.content, file.lastOp);
-	},
+    fileNames: function(project, callback) {
+        _server.list(project, '', callback);
+    },
 
-	removeFile: function(project, path, callback) {
-	    _server.removeFile(project, path, callback);
-	},
+    saveFile: function(project, file) {
+        // Unix files should always have a trailing new-line; add if not present
+        if (!file.content.endsWith("\n")) file.content += "\n";
 
-	whenFileExists: function(project, path, callbacks) {
-		_server.list(project, Path.directory(path), function(files) {
-			if (files && files.include(path)) {
-				callbacks['execute']();
-			} else {
-				if (callbacks['elseFailed']) callbacks['elseFailed']();
-			}
-		});
-	},
+        _server.saveFile(project, file.name, file.content, file.lastOp);
+    },
 
-	whenFileDoesNotExist: function(project, path, callbacks) {
-		_server.list(project, Path.directory(path), function(files) {
-			if (!files || !files.include(path)) {
-				callbacks['execute']();
-			} else {
-				if (callbacks['elseFailed']) callbacks['elseFailed']();
-			}
-		});
-	}
+    removeFile: function(project, path, callback) {
+        _server.removeFile(project, path, callback);
+    },
+
+    whenFileExists: function(project, path, callbacks) {
+        _server.list(project, Path.directory(path), function(files) {
+            if (files && files.include(path)) {
+                callbacks['execute']();
+            } else {
+                if (callbacks['elseFailed']) callbacks['elseFailed']();
+            }
+        });
+    },
+
+    whenFileDoesNotExist: function(project, path, callbacks) {
+        _server.list(project, Path.directory(path), function(files) {
+            if (!files || !files.include(path)) {
+                callbacks['execute']();
+            } else {
+                if (callbacks['elseFailed']) callbacks['elseFailed']();
+            }
+        });
+    }
 
 });
