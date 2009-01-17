8173f615f59d16817557e08004ca882ad9fc0cf0
Dion Almaer <dion@mozilla.com>
2009-01-17 13:44:25 -0800

    add back urlbar which seemed to get lost in kevins last merge; add test selection test getting ready for real thing; get rid of bad version of syntax highlighter which also found its way back

---
+++ frontend/js/util/urlbar.js
@@ -0,0 +1,21 @@
+/*
+ * URLBar watches the browser URL navigation bar for changes. 
+ * If it sees a change it tries to open the file
+ * The common case is using the back/forward buttons
+ */
+var URLBar = {
+        last: document.location.hash,
+        check: function() {
+                var hash = document.location.hash;
+                if (this.last != hash) {
+                        var urlchange = new URLSettings(hash);
+                        document.fire("bespin:editor:openfile", { filename: urlchange.get('path') });
+                        this.last = hash;
+                }
+        }
+};
+
+setInterval(function() {
+        URLBar.check.apply(URLBar);
+}, 200);
+
