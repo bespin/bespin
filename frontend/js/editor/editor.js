6e14ece1431f847cb425c766c8164a294b8e002c
Dion Almaer <dion@mozilla.com>
2009-01-26 12:31:50 -0800

    475370: double click on a word works now

--- frontend/js/editor/editor.js
+++ frontend/js/editor/editor.js
@@ -452,7 +452,17 @@ var EditorUI = Class.create({
             if (e.detail == 1) {
                 this.editor.setSelection(undefined);
             } else if (e.detail == 2) {
-                // select the word
+                var row = this.editor.model.rows[down.row];
+                var cursorAt = row[down.col];
+                if (!cursorAt || cursorAt.charAt(0) == ' ') { // empty space
+                    // For now, don't select anything, but think about copying Textmate and grabbing around it
+                } else {
+                    var startPos = up = this.editor.model.findBefore(down.row, down.col);
+                    
+                    var endPos = this.editor.model.findAfter(down.row, down.col);
+                    
+                    this.editor.setSelection({ startPos: startPos, endPos: endPos });
+                }
             } else if (e.detail > 2) {
                 // select the line
                 this.editor.setSelection({ startPos: { row: down.row, col: 0 }, endPos: { row: down.row + 1, col: 0 } });
@@ -464,7 +474,7 @@ var EditorUI = Class.create({
     },
 
     toggleCursor: function(ui) {
-        ui.showCursor = !(ui.showCursor);
+        ui.showCursor = !ui.showCursor;
         ui.editor.paint();
         setTimeout(function() { ui.toggleCursor(ui) }, 250);
     },
@@ -495,7 +505,7 @@ var EditorUI = Class.create({
         this.editor.model.clear();
         this.editor.model.insertCharacters({ row: 0, col: 0}, e.type);
     },
-
+    
     handleScrollBars: function(e) {
         var clientY = e.clientY - this.getTopOffset();
         var clientX = e.clientX - this.getLeftOffset();
