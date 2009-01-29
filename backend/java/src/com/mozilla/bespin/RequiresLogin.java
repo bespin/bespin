09621bdde9f0fac0ac620ed58687f5057bfc83f9
Dion Almaer <dion@mozilla.com>
2009-01-28 16:52:32 -0800

    big refactoring to give us command line as pluggable. now does simple commands in dashboard only

--- backend/java/src/com/mozilla/bespin/RequiresLogin.java
+++
@@ -1,7 +0,0 @@
-package com.mozilla.bespin;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface RequiresLogin {}
