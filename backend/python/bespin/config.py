e4eb3acf04d5cbd8bf3ba17a2f68482174ebafae
Kevin Dangoor <kdangoor@mozilla.com>
2009-01-30 20:53:29 -0500

    test_users is now passing with an SQLAlchemy-backed storage

--- backend/python/bespin/config.py
+++ backend/python/bespin/config.py
@@ -28,6 +28,9 @@
 
 import os
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 class Bunch(dict):
     def __getattr__(self, attr):
         try:
@@ -39,8 +42,7 @@ class Bunch(dict):
         self[attr] = value
 
 c = Bunch()
-c.users_db = None
-c.users_db_cache = None
+c.dburl = None
 c.file_db = None
 c.file_db_cache = None
 c.status_db = None
@@ -49,6 +51,7 @@ c.edit_db = None
 c.edit_db_cache = None
 c.secret = "This is the phrase that is used for secret stuff."
 c.static_dir = os.path.abspath("%s/../../../frontend" % os.path.dirname(__file__))
+c.sessionmaker = sessionmaker()
 
 c.max_import_file_size = 20000000
 
@@ -56,8 +59,6 @@ def set_profile(profile):
     if profile == "test":
         # this import will install the bespin_test store
         import bespin.tests.util
-        c.users_db = "bespin_test://"
-        c.users_db_cache = "simple://"
         c.file_db = "bespin_test://"
         c.file_db_cache = "simple://"
         c.status_db = "bespin_test://"
@@ -65,27 +66,26 @@ def set_profile(profile):
         c.edit_db = "bespin_test://"
         c.edit_db_cache = "simple://"
         c.saved_keys = dict()
+        c.dburl = "sqlite://"
     elif profile == "dev":
         if not os.path.exists("devdata"):
             os.mkdir("devdata")
-        c.users_db = "jsonbase://devdata/users"
-        c.users_db_cache = "memory://"
         c.file_db = "jsonfiles://devdata/files"
         c.file_db_cache = "memory://"
         c.status_db = "jsonbase://devdata/status"
         c.status_db_cache = "memory://"
         c.edit_db = "jsonbase://devdata/edits"
         c.edit_db_cache = "memory://"
+        c.dburl = "sqlite:///devdata.db"
+    c.dbengine = create_engine(c.dburl)
     
 def activate_profile():
     from shove import Shove
     from bespin.model import UserManager, FileManager, DB
     
-    c.user_manager = UserManager(Shove(c.users_db, c.users_db_cache))
     c.file_manager = FileManager(Shove(c.file_db, c.file_db_cache),
                                  Shove(c.status_db, c.status_db_cache),
                                  Shove(c.edit_db, c.edit_db_cache))
-    c.db = DB(c.user_manager, c.file_manager)
 
 def dev_spawning_factory(spawning_config):
     spawning_config['app_factory'] = spawning_config['args'][0]
