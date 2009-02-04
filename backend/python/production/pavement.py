32634e66f478a5d31553175ee631aa787b929420
Kevin Dangoor <kdangoor@mozilla.com>
2009-02-04 11:31:35 -0500

    getting things ready to build a production tarball

---
+++ backend/python/production/pavement.py
@@ -0,0 +1,15 @@
+from paver.defaults import *
+
+import paver.virtual
+
+options(
+    virtualenv=Bunch(
+        packages_to_install=['pip'],
+        paver_command_line="setup"
+    )
+)
+
+@task
+def setup():
+    """Get this production environment setup."""
+    sh("bin/pip install -U -r requirements.txt")
