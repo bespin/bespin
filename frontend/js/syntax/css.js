ad23412ab0fc51daf96c50da89a36fddefc18878
Dion Almaer <dion@mozilla.com>
2009-02-10 10:57:16 -0800

    add the hidden typingtest command

--- frontend/js/syntax/css.js
+++ frontend/js/syntax/css.js
@@ -39,84 +39,158 @@ if (!Bespin.Syntax) Bespin.Syntax = {};
 //     foo: bar;
 // }
 
+Bespin.Syntax.CSSConstants = {
+    C_STYLE_COMMENT: "c-comment",
+    STRING: "string",
+    KEYWORD: "keyword",
+    PUNCTUATION: "punctuation",
+    OTHER: "plain",
+    NAME: "attribute-name",
+    VALUE: "attribute-value",
+    IMPORTANT: "important"
+}
+
 Bespin.Syntax.CSSSyntaxEngine = Class.create({
-    punctuation: '{ } . , ; ( ) ? : = " \''.split(" "),
+	keywords: 'ascent azimuth background-attachment background-color background-image background-position ' +
+					'background-repeat background baseline bbox border-collapse border-color border-spacing border-style border-top ' +
+					'border-right border-bottom border-left border-top-color border-right-color border-bottom-color border-left-color ' +
+					'border-top-style border-right-style border-bottom-style border-left-style border-top-width border-right-width ' +
+					'border-bottom-width border-left-width border-width border cap-height caption-side centerline clear clip color ' +
+					'content counter-increment counter-reset cue-after cue-before cue cursor definition-src descent direction display ' +
+					'elevation empty-cells float font-size-adjust font-family font-size font-stretch font-style font-variant font-weight font ' +
+					'height letter-spacing line-height list-style-image list-style-position list-style-type list-style margin-top ' +
+					'margin-right margin-bottom margin-left margin marker-offset marks mathline max-height max-width min-height min-width orphans ' +
+					'outline-color outline-style outline-width outline overflow padding-top padding-right padding-bottom padding-left padding page ' +
+					'page-break-after page-break-before page-break-inside pause pause-after pause-before pitch pitch-range play-during position ' +
+					'quotes richness size slope src speak-header speak-numeral speak-punctuation speak speech-rate stemh stemv stress ' +
+					'table-layout text-align text-decoration text-indent text-shadow text-transform unicode-bidi unicode-range units-per-em ' +
+					'vertical-align visibility voice-family volume white-space widows width widths word-spacing x-height z-index'.split(" "),
+
+	values:	'above absolute all always aqua armenian attr aural auto avoid baseline behind below bidi-override black blink block blue bold bolder '+
+					'both bottom braille capitalize caption center center-left center-right circle close-quote code collapse compact condensed '+
+					'continuous counter counters crop cross crosshair cursive dashed decimal decimal-leading-zero default digits disc dotted double '+
+					'embed embossed e-resize expanded extra-condensed extra-expanded fantasy far-left far-right fast faster fixed format fuchsia '+
+					'gray green groove handheld hebrew help hidden hide high higher icon inline-table inline inset inside invert italic '+
+					'justify landscape large larger left-side left leftwards level lighter lime line-through list-item local loud lower-alpha '+
+					'lowercase lower-greek lower-latin lower-roman lower low ltr marker maroon medium message-box middle mix move narrower '+
+					'navy ne-resize no-close-quote none no-open-quote no-repeat normal nowrap n-resize nw-resize oblique olive once open-quote outset '+
+					'outside overline pointer portrait pre print projection purple red relative repeat repeat-x repeat-y rgb ridge right right-side '+
+					'rightwards rtl run-in screen scroll semi-condensed semi-expanded separate se-resize show silent silver slower slow '+
+					'small small-caps small-caption smaller soft solid speech spell-out square s-resize static status-bar sub super sw-resize '+
+					'table-caption table-cell table-column table-column-group table-footer-group table-header-group table-row table-row-group teal '+
+					'text-bottom text-top thick thin top transparent tty tv ultra-condensed ultra-expanded underline upper-alpha uppercase upper-latin '+
+					'upper-roman url visible wait white wider w-resize x-fast x-high x-large x-loud x-low x-slow x-small x-soft xx-large xx-small yellow' +
+					'monospace tahoma verdana arial helvetica sans-serif serif'.split(" "),	
+
+    important: '!important',
+
+    punctuation: '{ } / + % * . , ; ( ) ? : = " \''.split(" "),
 
     highlight: function(line, meta) {
-        var regions = {};
-        var currentStyle = (meta.inMultilineComment) ? "c-comment" : undefined;
-        var currentRegion = {};
+        var cssc = Bespin.Syntax.CSSConstants;    // aliasing the constants for shorter reference ;-)
+
+        var regions = {};                               // contains the individual style types as keys, with array of start/stop positions as value
+
+        // current state, maintained as we parse through each character in the line; values at any time should be consistent
+        var currentStyle = (meta.inMultilineComment) ? cssc.C_STYLE_COMMENT : undefined;
+        var currentRegion = {}; // should always have a start property for a non-blank buffer
         var buffer = "";
-        var multiline = false;  // this line contains an unterminated multi-line comment
-        var inRule = false;
+
+        // these properties are related to the parser state above but are special cases
+        var stringChar = "";    // the character used to start the current string
+        var multiline = meta.inMultilineComment;  // this line contains an unterminated multi-line comment
 
         for (var i = 0; i < line.length; i++) {
             var c = line.charAt(i);
 
             // check if we're in a comment and whether this character ends the comment
-            if (currentStyle == "c-comment") {
-                if (c == "/" && buffer.endsWith("*")) {
+            if (currentStyle == cssc.C_STYLE_COMMENT) {
+                if (c == "/" && buffer.endsWith("*")) { // has the c-style comment just ended?
                     currentRegion.stop = i;
                     this.addRegion(regions, currentStyle, currentRegion);
+                    currentRegion = {};
+                    currentStyle = undefined;
+                    multiline = false;
                     buffer = "";
+                } else {
+                    if (buffer == "") currentRegion = { start: i };
+                    buffer += c;
                 }
-                buffer += c;
+
                 continue;
             }
 
-
-
             if (this.isWhiteSpaceOrPunctuation(c)) {
-                if (buffer != "") {
-                    currentRegion.stop = i;
-
-                }
-
-                // check if we're in a selector
-                if (currentStyle == "selector") {
-                    if (c != "}") {
+                // check if we're in a string
+                if (currentStyle == cssc.STRING) {
+                    // if this is not an unescaped end quote (either a single quote or double quote to match how the string started) then keep going
+                    if ( ! (c == stringChar && !buffer.endsWith("\\"))) {
+                        if (buffer == "") currentRegion = { start: i };
                         buffer += c;
                         continue;
                     }
                 }
 
+                // if the buffer is full, add it to the regions
                 if (buffer != "") {
                     currentRegion.stop = i;
 
-                    if (currentStyle != "string") {
-                        if (this.keywords.indexOf(buffer.toLowerCase()) != -1) {
+                    if (currentStyle != cssc.STRING) {   // if this is a string, we're all set to add it; if not, figure out if its a keyword
+                        if (this.keywords.indexOf(buffer) != -1) {
                             // the buffer contains a keyword
-                            currentStyle = "keyword";
+                            currentStyle = cssc.KEYWORD;
+                        } else if (this.values.indexOf(buffer.toLowerCase()) != -1) {
+                            currentStyle = cssc.VALUE;
+                        } else if (this.important.indexOf(buffer) != -1) {
+                            currentStyle = cssc.IMPORTANT;
                         } else {
-                            currentStyle = "other";
+                            currentStyle = cssc.OTHER;
                         }
                     }
                     this.addRegion(regions, currentStyle, currentRegion);
+                    currentRegion = {};
+                    stringChar = "";
                     buffer = "";
+                    // i don't clear the current style here so I can check if it was a string below
                 }
 
                 if (this.isPunctuation(c)) {
-                    // add an ad-hoc region for just this thing
-                    this.addRegion(regions, "punctuation", { start: i, stop: i + 1 });
+                    if (c == "*" && i > 0 && (line.charAt(i - 1) == "/")) {
+                        // remove the previous region in the punctuation bucket, which is a forward slash
+                        regions[cssc.PUNCTUATION].pop();
+
+                        // we are in a c-style comment
+                        multiline = true;
+                        currentStyle = cssc.C_STYLE_COMMENT;
+                        currentRegion = { start: i - 1 };
+                        buffer = "/*";
+                        continue;
+                    }
+
+                    // add an ad-hoc region for just this one punctuation character
+                    this.addRegion(regions, cssc.PUNCTUATION, { start: i, stop: i + 1 });
                 }
 
-                if (currentStyle == "string") {
-                    currentStyle = "";
+                // find out if the current quote is the end or the beginning of the string
+                if ((c == "'" || c == '"') && (currentStyle != cssc.STRING)) {
+                    currentStyle = cssc.STRING;
+                    stringChar = c;
                 } else {
-                    if (c == "'" || c == '"') {
-                        currentStyle = "string";
-                        stringChar = c;
-                        currentRegion = { start: i + 1 };
-                    }
+                    currentStyle = undefined;
                 }
 
                 continue;
             }
 
-            if (buffer == "") {
-                currentRegion = { start: i };
-            }
+            if (buffer == "") currentRegion = { start: i };
             buffer += c;
+        }
+
+        // check for a trailing character inside of a string or a comment
+        if (buffer != "") {
+            if (!currentStyle) currentStyle = cssc.OTHER;
+            currentRegion.stop = line.length;
+            this.addRegion(regions, currentStyle, currentRegion);
         }
 
         return regions;
@@ -140,6 +214,5 @@ Bespin.Syntax.CSSSyntaxEngine = Class.create({
     }
 });
 
-
-
-// TODO: register this puppy
+// Register this puppy
+Bespin.Syntax.EngineResolver.register(new Bespin.Syntax.CSSSyntaxEngine(), ['css']);
