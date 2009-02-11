5f4c2875a4fec989cc5ba7a186f49107d89f88f0
Kevin Dangoor <kdangoor@mozilla.com>
2009-02-11 13:31:39 -0500

    the user object now keeps track of quotas, and file operations alter the amount used as needed.

--- backend/python/bespin/tests/test_files.py
+++ backend/python/bespin/tests/test_files.py
@@ -76,11 +76,15 @@ def _get_fm():
 
 def test_basic_file_creation():
     fm = _get_fm()
+    starting_point = macgyver.amount_used
     fm.save_file(macgyver, "bigmac", "reqs", "Chewing gum wrapper")
     file_obj = fm.session.query(model.File).filter_by(name="bigmac/reqs").one()
     data = str(file_obj.data)
     assert data == 'Chewing gum wrapper'
     assert file_obj.saved_size == 19
+    ending_point = macgyver.amount_used
+    difference = ending_point - starting_point
+    assert difference == 19
     
     now = datetime.now()
     assert now - file_obj.created < timedelta(seconds=2)
@@ -293,8 +297,10 @@ def test_can_delete_file_open_by_me():
     
 def test_successful_deletion():
     fm = _get_fm()
+    starting_used = macgyver.amount_used
     fm.save_file(macgyver, "bigmac", "foo/bar/baz", "biz")
     fm.delete(macgyver, "bigmac", "foo/bar/baz")
+    assert macgyver.amount_used == starting_used
     try:
         fm.get_file(macgyver, "bigmac", "foo/bar/baz")
         assert False, "Expected FileNotFound because the file is gone"
@@ -312,12 +318,17 @@ def test_top_level_deletion():
     
 def test_directory_deletion():
     fm = _get_fm()
+    fm.save_file(macgyver, "bigmac", "whiz/bang", "stillmore")
+    starting_used = macgyver.amount_used
     fm.save_file(macgyver, "bigmac", "foo/bar", "data")
+    fm.save_file(macgyver, "bigmac", "foo/blorg", "moredata")
     fm.delete(macgyver, "bigmac", "foo/")
     flist = fm.list_files(macgyver, "bigmac")
-    assert flist == []
+    assert len(flist) == 1
+    assert flist[0].name == 'bigmac/whiz/'
     file = fm.session.query(File).filter_by(name="bigmac/foo/bar").first()
     assert file is None
+    assert macgyver.amount_used == starting_used
     
 def test_project_deletion():
     fm = _get_fm()
