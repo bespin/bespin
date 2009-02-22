2ff9267e914e1918a8fe00027f99c2e0af50239c
Dion Almaer <dion@mozilla.com>
2009-02-22 02:18:02 -0800

    add support for creating your own commands now, including the commands to do this: cmdadd, cmdload, cmdedit, cmdlist, cmdrm

--- frontend/js/commandline/commandline.js
+++ frontend/js/commandline/commandline.js
@@ -131,7 +131,7 @@ Bespin.CommandLine.Interface = Class.create({
 
     showUsage: function(command, autohide) {
         var usage = command.usage || "no usage information found for " + command.name;
-        this.showInfo("Usage: " + usage, autohide);
+        this.showInfo("Usage: " + command.name + " " + usage, autohide);
     },
     
     showInfo: function(html, autohide) {
