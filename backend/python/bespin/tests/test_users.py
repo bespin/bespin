bfd14bc4f57873cfedf3f3abadbfee288cbbdd25
Kevin Dangoor <kdangoor@mozilla.com>
2009-02-24 20:55:24 -0500

    going to make username length match email length so that people can use their email addresses as their usernames if they wish

--- backend/python/bespin/tests/test_users.py
+++ backend/python/bespin/tests/test_users.py
@@ -197,5 +197,3 @@ def test_username_with_bad_characters():
             dict(password="foo", email="thinga@majig"), status=400)
     resp = app.post("/register/new/Thinga<majig>",
             dict(password="foo", email="thinga@majig"), status=400)
-    resp = app.post("/register/new/ReallyLongNameThatIsNotAllowed",
-            dict(password="foo", email="thinga@majig"), status=400)
