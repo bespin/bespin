c5b2d5242b7588ca0376b5d602994762360a66ab
Dion Almaer <dion@mozilla.com>
2009-04-15 14:34:39 -0700

    clean up

--- frontend/js/bespin/client/server.js
+++ frontend/js/bespin/client/server.js
@@ -131,7 +131,7 @@ dojo.declare("bespin.client.Server", null, {
     // that we know when we need to check for messages from the
     // server.
     asyncStarted: function() {
-        console.log("Starting new server-side async job.")
+        console.log("Starting new server-side async job.");
         if (this._asyncCount == 0) {
             this.processMessages();
         }
@@ -145,7 +145,7 @@ dojo.declare("bespin.client.Server", null, {
     // that we know when we can stop checking for messages from the
     // server.
     asyncEnded: function() {
-        console.log("Server-side async job done.")
+        console.log("Server-side async job done.");
         if (this._asyncCount > 0) {
             this._asyncCount--;
         }
@@ -450,7 +450,6 @@ dojo.declare("bespin.client.Server", null, {
     // * {{{archivetype}}} is either zip | tgz
     exportProject: function(project, archivetype) {
         if (bespin.util.include(['zip','tgz','tar.gz'], archivetype)) {
-//            console.log('/project/export', project + "." + archivetype);
             var iframe = document.createElement("iframe");
             iframe.src = bespin.util.path.combine('/project/export', project + "." + archivetype);
             iframe.style.display = 'none';
