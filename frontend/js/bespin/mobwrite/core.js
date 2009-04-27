d08630e5229bbceaad0efff4d0ad0f394c964a54
Joe Walker <jwalker@mozilla.com>
2009-04-27 14:53:02 +0100

    making mobwrite play along with the csrf fix

--- frontend/js/bespin/mobwrite/core.js
+++ frontend/js/bespin/mobwrite/core.js
@@ -687,6 +687,11 @@ mobwrite.syncLoadAjax_ = function(url, post, callback) {
     req.onreadystatechange = callback;
     req.open('POST', url, true);
     req.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+
+    // CSRF protection as defined by Bespin
+    var token = bespin.get("server").token;
+    req.setRequestHeader("Domain-Token", token);
+
     req.send(post);
   }
   return req;
