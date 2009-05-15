67062b9635f58ac13b8e6cd7e755f851bfaa3e7d
Joe Walker <jwalker@mozilla.com>
2009-05-15 16:59:13 +0100

    making drawing area work (a bit)

--- frontend/js/bespin/editor/piemenu.js
+++ frontend/js/bespin/editor/piemenu.js
@@ -25,68 +25,68 @@
 dojo.provide("bespin.editor.piemenu");
 dojo.require("dojo.fx.easing");
 
-dojo.declare("bespin.editor.piemenu.Window", th.components.Panel, {
+dojo.declare("bespin.editor.piemenu.Window", null, {
     constructor: function() {
         this.isVisible = false;
         this.editor = bespin.get("editor");
 
-        this.menu = dojo.create("canvas", {
+        this.canvas = dojo.create("canvas", {
             id: 'piemenu',
-            style: "position:absolute; z-index:100; top:64px; border:0px solid red; display:none;"
+            style: "position:absolute; z-index:100; top:64px; display:none;"
         }, dojo.body());
-        this.menu.height = this.editor.canvas.height;
-        this.menu.width = this.editor.canvas.width;
-        this.ctx = this.menu.getContext('2d');
+        this.canvas.height = this.editor.canvas.height;
+        this.canvas.width = this.editor.canvas.width;
+        this.ctx = this.canvas.getContext('2d');
 
-        this.images = {};
-        var ids = [
-            "active_btm", "active_lft", "active_rt", "active_top", "off",
-            "menu_btm_lft", "menu_btm_lftb", "menu_btm_rt", "menu_btm_rtb",
-            "menu_lft", "menu_mid", "menu_rt", "menu_top_lft",
-            "menu_top_mid", "menu_top_rt"
-        ];
-        dojo.forEach(ids, function(id) {
-            this.images[id] = dojo.create("img", {
-                id: "puck_" + id,
-                src: "/images/pie/puck_" + id + ".png",
+        this.slices = {
+            btm: { id: "active_btm", showContents: this.showCommand },
+            top: { id: "active_top", showContents: this.showFiles },
+            lft: { id: "active_lft", showContents: this.showReference },
+            rt: { id: "active_rt", showContents: this.showContext },
+            off: { id: "off", showContents: this.hideDetail }
+        };
+
+        for (var dir in this.slices) {
+            var slice = this.slices[dir];
+            slice.img = dojo.create("img", {
+                id: "puck_" + slice.id,
+                src: "/images/pie/puck_" + slice.id + ".png",
+                alt: "pie menu",
+                style: "position:absolute; display:none;"
+            }, dojo.body());
+        }
+
+        this.border = [];
+        var borderIds = [ "lft", "mid", "rt", "top_lft", "top_mid", "top_rt", "btm_lft", "btm_lftb", "btm_rt", "btm_rtb" ];
+        dojo.forEach(borderIds, function(id) {
+            this.border[id] = dojo.create("img", {
+                id: "puck_menu" + id,
+                src: "/images/pie/puck_menu_" + id + ".png",
                 alt: "pie menu",
                 style: "position:absolute; display:none;"
             }, dojo.body());
         }, this);
 
-        /*
-        this.input = dojo.create("input", {
-            type: "text",
-            id: "piemenu_text"
-        }, dojo.body());
-        */
-
         var self = this;
 
         /*
-        dojo.connect(this, "mousedown", function(ev) {
-            if (!self.isVisible) {
-                return;
-            }
+        dojo.connect(window, "mousedown", function(ev) {
+            if (!self.isVisible) return;
 
-            var pos = dojo.coords(self.container);
+            var pos = dojo.coords(self.canvas);
             if (ev.clientX < pos.l
                     || ev.clientX > (pos.l + pos.w)
                     || ev.clientY < pos.t
                     || ev.clientY > (pos.t + pos.h)) {
                 self.toggle();
-            } else {
-                dojo.stopEvent(ev);
             }
+            dojo.stopEvent(ev);
         });
         */
 
-        dojo.connect(this, "keydown", function(ev) {
+        dojo.connect(window, "keydown", function(ev) {
+            if (!self.isVisible) return;
             var key = bespin.util.keys.Key;
-
-            if (!self.isVisible) {
-                return;
-            }
 
             console.log("pie keydown", key);
 
@@ -94,11 +94,27 @@ dojo.declare("bespin.editor.piemenu.Window", th.components.Panel, {
                 self.toggle();
                 dojo.stopEvent(ev);
             }
+            else if (ev.keyCode == key.UP_ARROW) {                
+                self.renderPopout(self.slices.top);
+                dojo.stopEvent(ev);
+            }
+            else if (ev.keyCode == key.DOWN_ARROW) {
+                self.renderPopout(self.slices.btm);
+                dojo.stopEvent(ev);
+            }
+            else if (ev.keyCode == key.RIGHT_ARROW) {
+                self.renderPopout(self.slices.rt);
+                dojo.stopEvent(ev);
+            }
+            else if (ev.keyCode == key.LEFT_ARROW) {
+                self.renderPopout(self.slices.lft);
+                dojo.stopEvent(ev);
+            }
         });
     },
 
     show: function() {
-        var self = this;        
+        var self = this;
         var anim = dojo.fadeIn({
             node: {
                 style:{}
@@ -110,11 +126,13 @@ dojo.declare("bespin.editor.piemenu.Window", th.components.Panel, {
                 self.renderPie(progress);
             },
             onEnd: function() {
-                self.renderPopout();
+                console.log("where do you want to go today?");
+                self.canvas.focus();
             }
         });
 
-        this.menu.style.display = 'block';
+        this.canvas.style.display = 'block';
+        this.canvas.focus();
         anim.play();
     },
 
@@ -131,7 +149,7 @@ dojo.declare("bespin.editor.piemenu.Window", th.components.Panel, {
                 self.renderPie(progress);
             },
             onEnd: function() {
-                self.menu.style.display = 'none';
+                self.canvas.style.display = 'none';
             },
         });
         anim.play();
@@ -139,21 +157,21 @@ dojo.declare("bespin.editor.piemenu.Window", th.components.Panel, {
 
     renderPie: function(progress) {
         var ctx = this.ctx;
-        var off = this.images.off;
+        var off = this.slices.off.img;
 
         ctx.save();
 
-        ctx.clearRect(0, 0, this.menu.width, this.menu.height);
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         var alpha = Math.max(progress - 0.4, 0);
         ctx.fillStyle = "rgba(0, 0, 0, " + alpha + ")";
-        ctx.fillRect(0, 0, this.menu.width, this.menu.height);
+        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
         var height = parseInt(off.height * progress);
         var width = parseInt(off.width * progress);
 
-        var x = parseInt((this.menu.width / 2) - (width / 2));
-        var y = parseInt((off.height - height) / 2) + this.menu.height - off.height;
+        var x = parseInt((this.canvas.width / 2) - (width / 2));
+        var y = parseInt((off.height - height) / 2) + this.canvas.height - off.height;
 
         var xm = x + (width / 2);
         var ym = y + (height / 2);
@@ -178,61 +196,97 @@ dojo.declare("bespin.editor.piemenu.Window", th.components.Panel, {
      * Shrink border images
      * Many of the images are dups. we should save load time
      * - Also consider rotational and translational sym??
+     * Make the canvas cover the screen and not the editor area
      */
-    renderPopout: function() {
+    renderPopout: function(active) {
         var margin = 10;
-        var active = this.images.active_btm;
+
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.fillStyle = "rgba(0, 0, 0, 0.6)";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
         // Left hand edge of pie. Determined by height of pie
-        var offLeft = parseInt((this.menu.width / 2) - (active.width / 2));
+        var offLeft = parseInt((this.canvas.width / 2) - (active.img.width / 2));
         // Top of bottom row. Determined by height of pie
-        var btmTop = this.menu.height - active.height;
+        var btmTop = this.canvas.height - active.img.height;
         // Left hand edge of rightmost column. Assumes all RHS graphics are the same width
-        var rightLeft = this.menu.width - margin - this.images.menu_top_rt.width;
+        var rightLeft = this.canvas.width - margin - this.border.top_rt.width;
         // Top of all middle rows. Assumes all top graphics are same height
-        var midTop = margin + this.images.menu_top_mid.height;
+        var midTop = margin + this.border.top_mid.height;
         // Left hand edge of center column. Assumes all LHS graphics are same width
-        var cenLeft = margin + this.images.menu_top_lft.width;
+        var cenLeft = margin + this.border.top_lft.width;
         // Height of the middle row. Assumes all top graphics are same height
-        var midHeight = btmTop - margin - this.images.menu_top_mid.height;
+        var midHeight = btmTop - margin - this.border.top_mid.height;
         // Width of the center column. Assumes left and right columns graphics are same width
-        var cenWidth = this.menu.width - (margin + this.images.menu_top_lft.width) - (margin + this.images.menu_top_rt.width);
+        var cenWidth = this.canvas.width - (margin + this.border.top_lft.width) - (margin + this.border.top_rt.width);
 
         // top left
-        this.ctx.drawImage(this.images.menu_top_lft, margin, margin);
+        this.ctx.drawImage(this.border.top_lft, margin, margin);
 
         // top center
-        this.ctx.drawImage(this.images.menu_top_mid, cenLeft, margin, cenWidth, this.images.menu_top_mid.height);
+        this.ctx.drawImage(this.border.top_mid, cenLeft, margin, cenWidth, this.border.top_mid.height);
 
         // top right
-        this.ctx.drawImage(this.images.menu_top_rt, rightLeft, margin);
+        this.ctx.drawImage(this.border.top_rt, rightLeft, margin);
 
         // middle left
-        this.ctx.drawImage(this.images.menu_lft, margin, midTop, this.images.menu_lft.width, midHeight);
+        this.ctx.drawImage(this.border.lft, margin, midTop, this.border.lft.width, midHeight);
 
         // middle center display area
-        this.ctx.drawImage(this.images.menu_mid, cenLeft, midTop, cenWidth, midHeight);
+        this.ctx.drawImage(this.border.mid, cenLeft, midTop, cenWidth, midHeight);
 
         // middle right
-        this.ctx.drawImage(this.images.menu_rt, rightLeft, midTop, this.images.menu_rt.width, midHeight);
+        this.ctx.drawImage(this.border.rt, rightLeft, midTop, this.border.rt.width, midHeight);
 
         // bottom left
-        this.ctx.drawImage(this.images.menu_btm_lft, margin, btmTop);
+        this.ctx.drawImage(this.border.btm_lft, margin, btmTop);
 
         // bottom (left of pie)
-        var lftbWidth = offLeft - (margin + this.images.menu_btm_lft.width);
-        this.ctx.drawImage(this.images.menu_btm_lftb, cenLeft, btmTop, lftbWidth, this.images.menu_btm_lftb.height);
+        var lftbWidth = offLeft - (margin + this.border.btm_lft.width);
+        this.ctx.drawImage(this.border.btm_lftb, cenLeft, btmTop, lftbWidth, this.border.btm_lftb.height);
 
         // pie
-        this.ctx.drawImage(active, offLeft, btmTop);
+        this.ctx.drawImage(active.img, offLeft, btmTop);
 
         // bottom (right of pie)
-        var rtbLeft = offLeft + this.images.off.width;
+        var rtbLeft = offLeft + this.slices.off.img.width;
         var rtbWidth = rightLeft - (rtbLeft);
-        this.ctx.drawImage(this.images.menu_btm_rtb, rtbLeft, btmTop, rtbWidth, this.images.menu_btm_rtb.height);
+        this.ctx.drawImage(this.border.btm_rtb, rtbLeft, btmTop, rtbWidth, this.border.btm_rtb.height);
 
         // bottom right
-        this.ctx.drawImage(this.images.menu_btm_rt, rightLeft, btmTop);
+        this.ctx.drawImage(this.border.btm_rt, rightLeft, btmTop);
+
+        active.showContents.apply(this, [{ l:cenLeft, t:midTop, w:cenWidth, h:midHeight}]);
+    },
+
+    showCommand: function(coords) {
+        console.log("command line goes here");
+    },
+
+    showFiles: function(coords) {
+        this.ctx.fillText("File System", coords.l, coords.t);
+    },
+
+    showReference: function(coords) {
+        if (!this.refNode) {
+            this.refNode = dojo.create("iframe", {
+                id: "pie_ref",
+                src: "https://developer.mozilla.org/En/Canvas_tutorial/Using_images",
+                style: "z-index: 200"
+            }, dojo.body());
+        }
+        this.refNode.left = coords.l;
+        this.refNode.top = coords.t;
+        this.refNode.width = coords.w;
+        this.refNode.height = coords.h;
+    },
+
+    showContext: function(coords) {
+        console.log("context goes here");
+    },
+
+    hideDetail: function() {
+        console.log("hideDetail");
     },
 
     toggle: function() {
