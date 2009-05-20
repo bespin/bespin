770fd487ab6fe3fc4317569da84b53b14d3db822
Kevin Dangoor <kdangoor@mozilla.com>
2009-05-20 14:16:19 -0400

    add create_db to the production pavement

--- backend/python/production/pavement.py
+++ backend/python/production/pavement.py
@@ -29,3 +29,16 @@ def db():
     # touch the wsgi app so that mod_wsgi sees that we've updated
     sh("touch ../wsgi-apps/bespin.wsgi")
 
+@task
+def create_db():
+    """Creates the production database (requires ../wsgi-apps/bespin.wsgi to set up config)"""
+    from bespin import config, database, db_versions
+    from migrate.versioning.shell import main
+    
+    execfile("../wsgi-apps/bespin.wsgi", {'__file__' : '/home/wsgiapps/wsgi-apps/bespin.wsgi'})
+
+    dry("Create database tables", database.Base.metadata.create_all, bind=config.c.dbengine)
+    
+    repository = str(path(db_versions.__file__).dirname())
+    dburl = config.c.dburl
+    dry("Turn on migrate versioning", main, ["version_control", dburl, repository])
