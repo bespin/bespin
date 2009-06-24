a3860e5251549da5a03fa608841ab6008e5df61e
Dion Almaer <dion@mozilla.com>
2009-06-24 13:45:43 -0700

    default a new file to no spaces at all :)

--- frontend/js/bespin/client/filesystem.js
+++ frontend/js/bespin/client/filesystem.js
@@ -125,7 +125,7 @@ dojo.declare("bespin.client.FileSystem", null, {
     forceOpenFile: function(project, path, content) {
         this.whenFileDoesNotExist(project, path, {
             execute: function() {
-                if (!content) content = " ";
+                if (!content) content = "";
                 bespin.publish("editor:newfile", {
                     project: project,
                     newfilename: path,
